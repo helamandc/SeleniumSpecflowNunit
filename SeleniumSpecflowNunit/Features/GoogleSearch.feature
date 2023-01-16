9ad5c4c563e6a6318fd2d7432e80fe164e79691b
Helaman Del Castillo <helaman.delcastillo@praemium.com>
2023-01-16 17:19:46 +1100

    Add project files.

---
+++ SeleniumSpecflowNunit/Features/GoogleSearch.feature
@@ -0,0 +1,9 @@
+﻿Feature: Search something in Google
+
+This is to test the search functionality of Google search engine.
+
+Scenario: A random user opens Google website and search for Praemium
+	Given I open Google search engine
+	And I type Praemium in the search field
+	When I click on Google Search button
+	Then I would see results about Praemium
---
+++ SeleniumSpecflowNunit/StepDefinitions/SearchSomethingInGoogleStepDefinitions.cs
@@ -0,0 +1,59 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Net;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace SeleniumSpecflowNunit.StepDefinitions
+{
+    [Binding]
+    public class SearchSomethingInGoogleStepDefinitions
+    {
+
+        string test_url = "https://www.google.com/";
+        IWebDriver driver;
+        string itemname = "Praemium";
+        string itemURL = "https://www.praemium.com/";
+
+        [Given(@"I open Google search engine")]
+        public void GivenIOpenGoogleSearchEngine()
+        {
+            driver = new ChromeDriver();
+            driver.Url = test_url;
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
+        }
+
+        [Given(@"I type (.*) in the search field")]
+        public void GivenITypeInTheSearchField(string text)
+        {
+            IWebElement search = driver.FindElement(By.ClassName("gLFyf"));
+            search.Click();
+            search.Clear();
+            search.SendKeys(itemname);
+        }
+
+        [When(@"I click on Google Search button")]
+        public void WhenIClickOnGoogleSearchbutton()
+        {
+            IWebElement click = driver.FindElement(By.ClassName("gNO89b"));
+            click.Click();
+        }
+
+        [Then(@"I would see results about Praemium")]
+        public void ThenIWouldSeeResultsAboutPraemium()
+        {
+            IWebElement itemtext = driver.FindElement(By.ClassName("d8lRkd"));
+            string gettext = itemtext.Text;
+            Assert.That(gettext.Contains(itemURL), Is.True);
+        }
+
+        [AfterScenario]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
